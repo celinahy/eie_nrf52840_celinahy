78208682849cdd216c86f0252c5d8761395ecef6
celinahy <113138309+celinahy@users.noreply.github.com>
2025-11-27 20:00:47 -0700

    base code from ble peripheral week following steps

--- app/src/main.c
+++ app/src/main.c
@@ -2,27 +2,126 @@
  * main.c
  */
 
-#include <inttypes.h>
+/* IMPORTS -------------------------------------------------------------------------------------- */
 
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
 #include <zephyr/sys/printk.h>
 
-#include "BTN.h"
-#include "LED.h"
+/* MACROS --------------------------------------------------------------------------------------- */
 
-#define SLEEP_MS 1
+#define BLE_CUSTOM_SERVICE_UUID \
+  BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+#define BLE_CUSTOM_CHARACTERISTIC_UUID \
+  BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef2)
 
-int main(void) {
+#define BLE_CUSTOM_CHARACTERISTIC_MAX_DATA_LENGTH 20
 
-  if (0 > BTN_init()) {
+/* PROTOTYPES ----------------------------------------------------------------------------------- */
+
+static ssize_t ble_custom_characteristic_read_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                                                 void *buf, uint16_t len, uint16_t offset);
+static ssize_t ble_custom_characteristic_write_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                                                  const void *buf, uint16_t len, uint16_t offset,
+                                                  uint8_t flags);
+
+/* VARIABLES ------------------------------------------------------------------------------------ */
+
+static const struct bt_uuid_128 ble_custom_service_uuid = BT_UUID_INIT_128(BLE_CUSTOM_SERVICE_UUID);
+static const struct bt_uuid_128 ble_custom_characteristic_uuid = BT_UUID_INIT_128(BLE_CUSTOM_CHARACTERISTIC_UUID);
+
+static const struct bt_data ble_advertising_data[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BLE_CUSTOM_SERVICE_UUID),
+};
+
+static const struct bt_data ble_scan_response_data[] = {
+    BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+static uint8_t ble_custom_characteristic_user_data[BLE_CUSTOM_CHARACTERISTIC_MAX_DATA_LENGTH + 1] = {};
+
+/* BLE SERVICE SETUP ---------------------------------------------------------------------------- */
+
+BT_GATT_SERVICE_DEFINE(
+    ble_custom_service,                                // Name of the struct that will store the config for this service
+    BT_GATT_PRIMARY_SERVICE(&ble_custom_service_uuid), // Setting the service UUID
+
+    BT_GATT_CHARACTERISTIC(
+        &ble_custom_characteristic_uuid.uuid,   // Setting the characteristic UUID
+        BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE, // Possible operations
+        BT_GATT_PERM_READ | BT_GATT_PERM_WRITE, // Permissions that connecting devices have
+        ble_custom_characteristic_read_cb,      // Callback for when this characteristic is read from
+        ble_custom_characteristic_write_cb,     // Callback for when this characteristic is written to
+        ble_custom_characteristic_user_data     // Initial data stored in this characteristic
+        ),
+
+    // End of service definition
+);
+
+/* FUNCTIONS ------------------------------------------------------------------------------------ */
+
+static ssize_t ble_custom_characteristic_read_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                                                 void *buf, uint16_t len, uint16_t offset)
+{
+  const char *value = attr->user_data;
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, value, strlen(value));
+}
+
+static ssize_t ble_custom_characteristic_write_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                                                  const void *buf, uint16_t len, uint16_t offset,
+                                                  uint8_t flags)
+{
+  uint8_t *value_ptr = attr->user_data;
+
+  if (offset + len > BLE_CUSTOM_CHARACTERISTIC_MAX_DATA_LENGTH)
+  {
+    return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+  }
+
+  memcpy(value_ptr + offset, buf, len);
+  value_ptr[offset + len] = 0;
+
+  return len;
+}
+
+/* MAIN ----------------------------------------------------------------------------------------- */
+
+int main(void)
+{
+  int err = bt_enable(NULL);
+  if (err)
+  {
+    printk("Bluetooth init failed (err %d)\n", err);
     return 0;
   }
-  if (0 > LED_init()) {
+  else
+  {
+    printk("Bluetooth initialized!\n");
+  }
+
+  err =
+      bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ble_advertising_data, ARRAY_SIZE(ble_advertising_data),
+                      ble_scan_response_data, ARRAY_SIZE(ble_scan_response_data));
+  if (err)
+  {
+    printk("Advertising failed to start (err %d)\n", err);
     return 0;
   }
 
-  while(1) {
-    k_msleep(SLEEP_MS);
+  while (1)
+  {
+    k_sleep(K_MSEC(1000));
   }
-	return 0;
 }
